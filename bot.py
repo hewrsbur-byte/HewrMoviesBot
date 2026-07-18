a71280754eea93e9986513f56df90842a98933d2
hewrsbur-byte <hewr.sbur@gmail.com>
2026-07-18 20:58:13 +0300

    Implement basic Telegram bot with /start command
    Added a Telegram bot that responds to the /start command in Kurdish.

--- bot.py
+++ bot.py
@@ -1,1 +1,16 @@
+from telegram.ext import Updater, CommandHandler
+
+def start(update, context):
+    update.message.reply_text('سڵاو! من بۆتەکەم کاردەکات.')
+
+def main():
+    # لێرە تۆکێنەکەت دادەنێین
+    updater = Updater("8989364985:AAEwBkvkNjYmxl_GsEXlZPOkuQM50OFagwQ", use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
 
